aefec138190283c4e52f117f8f74f5c92ab85901
TonyMontana <anton.haiduk.job@gmail.com>
2026-05-05 17:28:59 +0200

    configure request rate limiter with Redis

--- gateway-service/src/main/java/com/gatewayservice/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/gatewayservice/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.gatewayservice.config;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -18,5 +22,12 @@ public class SecurityConfig {
                         .anyExchange().permitAll()
                 )
                 .build();
+    }
+
+    @Bean
+    public KeyResolver userIpKeyResolver() {
+        return exchange -> Mono.just(
+                Objects.requireNonNull(exchange.getRequest().getRemoteAddress()).getAddress().getHostAddress()
+        );
     }
 }
