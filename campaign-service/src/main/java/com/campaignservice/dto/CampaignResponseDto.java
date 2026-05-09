a050769fc219cd6c39fb39f49f60f82f8716a476
TonyMontana <anton.haiduk.job@gmail.com>
2026-05-09 15:20:39 +0200

    resolve polymorphic deserialization for DTO records

--- campaign-service/src/main/java/com/campaignservice/dto/CampaignResponseDto.java
+++ campaign-service/src/main/java/com/campaignservice/dto/CampaignResponseDto.java
@@ -1,10 +1,12 @@
 package com.campaignservice.dto;
 
 import com.campaignservice.model.Status;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
 public record CampaignResponseDto(
         Long id,
         String tokenName,
--- campaign-service/src/main/java/com/campaignservice/model/Campaign.java
+++ campaign-service/src/main/java/com/campaignservice/model/Campaign.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Setter
 @Getter
+@Table(name = "campaigns")
 public class Campaign {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
