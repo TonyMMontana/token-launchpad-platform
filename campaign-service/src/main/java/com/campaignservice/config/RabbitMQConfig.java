859089799894b6a8b895d249b4efd98e0b580cbc
Anton <anton.haiduk.job@gmail.com>
2026-06-08 21:02:48 +0300

    feature/k8s init (#8)
    * created dockerfile for services
    
    * add kubernetes base manifests and deployment docs

--- campaign-service/src/main/java/com/campaignservice/config/RabbitMQConfig.java
+++ campaign-service/src/main/java/com/campaignservice/config/RabbitMQConfig.java
@@ -67,7 +67,6 @@ public class RabbitMQConfig {
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
         template.setMessageConverter(messageConverter);
-        template.setMandatory(true);
         template.setConfirmCallback(
                 (correlationData, ack, cause) -> {
                     if (ack) {
